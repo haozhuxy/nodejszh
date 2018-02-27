879a045b63a6cbba49bf93bfb7db2182fc2a8ee7
Your Name <you@example.com>
2018-02-27 14:35:36 +0800

    注册

--- zhzh/routes/index.js
+++ zhzh/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+var UserModel=require("../model/UserModel");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: '登录画面' });
+  res.render('regist', { title: '注册' });
 });
 
+router.post("/api/regist",function(req,res){
+	var username=req.body.username;
+	var psw=req.body.psw;
+	
+	var um=new UserModel();
+	um.username=username;
+	um.psw=psw;
+	um.save(function(err){
+		if(err){
+			console.log("注册失败",err);
+			res.send("注册失败");
+		}else{
+			console.log("注册成功");
+			res.send("注册成功");
+		}
+	})
+})
 module.exports = router;
