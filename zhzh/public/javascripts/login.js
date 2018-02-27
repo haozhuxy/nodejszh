07719486f5260069902bf4f18198115fb4a04546
Your Name <you@example.com>
2018-02-27 20:15:28 +0800

    注册登录

---
+++ zhzh/public/javascripts/login.js
@@ -0,0 +1,32 @@
+//随机验证码
+$(function(){
+
+	var arr = 	["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+	"p","q","e","s","t","u","v","w","x","y","z","0","1","2","3",
+	"4","5","6","7","8","9","0","A","B","C","D","E","F","G","H",
+	"I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W",
+	"X","Y","Z","1","2","3","4","5","6","7","8","9"];
+
+	var sum = "";
+	for(i=0; i<4; i++){
+		var krr = arr[parseInt(Math.random()*72)];
+		    sum = sum + krr;	 		
+	}
+	
+	$(".ver").html(sum);
+		
+	$(".ver").click(function(){
+		var str = "";
+		for(var i=0; i<4;i++){
+			var m = parseInt(Math.random()*72);	
+			str = arr[m] + str;
+		}
+		$(this).html(str);
+	})
+
+
+
+
+})
+
+
--- zhzh/routes/index.js
+++ zhzh/routes/index.js
@@ -1,26 +1,71 @@
 var express = require('express');
 var router = express.Router();
+var md5=require("md5");
 var UserModel=require("../model/UserModel");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('regist', { title: '注册' });
 });
 
-router.post("/api/regist",function(req,res){
+router.get('/login',function(req,res){
+	res.render("login",{});
+});
+
+router.post("/api/login",function(req,res){
 	var username=req.body.username;
-	var psw=req.body.psw;
+	var psw=md5(req.body.psw);
+	var result={
+		  status:1,
+		  message:"登录成功"
+	}
+	UserModel.find({username:username,psw:psw},function(err,docs){
+		if(!err&&docs.length>0){
+			 console.log("登录成功");
+			res.send(result);
+		}else{
+			console.log("登录失败,请检查用户名或密码");
+			result.status=-109;
+			result.message="登录失败,请检查用户名或密码";
+			res.send(result);
+		}
+	})
+})
+
+router.post("/api/regist",function(req,res){
+	var result={
+		status:1,
+		message:"注册成功"
+	}
+	var username=req.body.username;
+	var psw=md5(req.body.psw);
 	
-	var um=new UserModel();
+	//用户名是否被使用查询
+	UserModel.find({username:username},function(err,docs){
+		if(!err&&docs.length>0){
+			console.log("用户名已经被使用,请选择别的用户名");
+			result.status=-111;
+			result.message="用户名已经被使用,请选择别的用户名";
+			res.send(result);
+			return;
+		}
+		
+		var um=new UserModel();
 	um.username=username;
 	um.psw=psw;
 	um.save(function(err){
 		if(err){
 			console.log("注册失败",err);
-			res.send("注册失败");
+			result.status=-110;
+			result.message="注册失败";
+			res.send(result);
 		}else{
 			console.log("注册成功");
-			res.send("注册成功");
+			res.send(result);
 		}
 	})
+		
+	})
+	
+	
 })
 module.exports = router;
